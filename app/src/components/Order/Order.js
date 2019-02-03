1ebe580258ef51a3b950c51cb7bfeb1ab6966a8f
Mateusz Buczek <this.mateuszbuczek@gmail.com>
2019-02-03 21:34:56 +0100

    orders page with data fetching from firebase

---
+++ app/src/components/Order/Order.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import classes from "./Order.css";
+
+const Order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({ name: ingredientName, amount: props.ingredients[ingredientName] });
+  }
+  return (
+    <div className={classes.Order}>
+      <p>
+        Ingredients:{" "}
+        {ingredients.map(ig => {
+          return (
+            <span
+              style={{
+                textTransform: "capitalize",
+                display: "inline-block",
+                margin: "0 8px",
+                border: "1px solid #ccc",
+                padding: "5px"
+              }}
+              key={ig.name}
+            >
+              {ig.name} ({ig.amount})
+            </span>
+          );
+        })}
+      </p>
+      <p>
+        Price: <strong>{props.price.toFixed(2) + " USD"}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Order;
