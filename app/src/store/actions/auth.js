3ffc447def381637a8f6ad94dbd4e9227104ec24
Mateusz Buczek <this.mateuszbuczek@gmail.com>
2019-02-05 08:20:54 +0100

    authentication with firebase, applied some restricted resources

---
+++ app/src/store/actions/auth.js
@@ -0,0 +1,65 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+    let url =
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAUaUyRdevmy_Cd3i5aQFJeDZjOvhluGCE";
+    if (!isSignup) {
+      url =
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAUaUyRdevmy_Cd3i5aQFJeDZjOvhluGCE";
+    }
+    axios
+      .post(url, authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data.idToken, res.data.localId));
+        dispatch(checkAuthTimeout(res.data.expiresIn));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err.response.data.error));
+      });
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
